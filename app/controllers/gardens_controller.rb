22e50b628990c074ba893347d5662f65fe544c38
Guillaume Jauffret <guillaumejfrt@gmail.com>
2018-11-22 11:29:12 +0100

    explanations for students

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,60 +1,51 @@
 class GardensController < ApplicationController
-  # before_action :set_garden, only: [:show, :edit, :update, :destroy]
+  before_action :set_garden, only: [:show, :edit, :update, :destroy]
 
-  # GET /gardens
-  # GET /gardens.json
+  # GET "/gardens"
   def index
     @gardens = Garden.all
   end
 
-  # GET /gardens/1
-  # GET /gardens/1.json
+  # GET "/gardens/:id"
   def show
-    @garden = Garden.find(params[:id])
+    # @garden is provided by the before_action method :set_garden
+    # :set_garden is called before the show action
+    # :set_garden is located at the end of the file under private
+
+    # We provide @plant as a new instance so that simple_form knows that it
+    # will have to POST on the create action of the PlantsController
     @plant = Plant.new
   end
 
-  # GET /gardens/new
+  # GET "/gardens/new"
   def new
     @garden = Garden.new
   end
 
-  # GET /gardens/1/edit
+  # GET "/gardens/:id/edit"
   def edit
   end
 
-  # POST /gardens
-  # POST /gardens.json
+  # POST "/gardens"
   def create
     @garden = Garden.new(garden_params)
-
-    respond_to do |format|
-      if @garden.save
-        format.html { redirect_to @garden, notice: 'Garden was successfully created.' }
-        format.json { render :show, status: :created, location: @garden }
-      else
-        format.html { render :new }
-        format.json { render json: @garden.errors, status: :unprocessable_entity }
-      end
+    if @garden.save
+      redirect_to @garden
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /gardens/1
-  # PATCH/PUT /gardens/1.json
+  # PATCH "/gardens/:id"
   def update
-    respond_to do |format|
-      if @garden.update(garden_params)
-        format.html { redirect_to @garden, notice: 'Garden was successfully updated.' }
-        format.json { render :show, status: :ok, location: @garden }
-      else
-        format.html { render :edit }
-        format.json { render json: @garden.errors, status: :unprocessable_entity }
-      end
+    if @garden.update(garden_params)
+      redirect_to @garden
+    else
+      render :edit
     end
   end
 
-  # DELETE /gardens/1
-  # DELETE /gardens/1.json
+  # DELETE "/gardens/:id"
   def destroy
     @garden.destroy
     respond_to do |format|
--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,9 @@
 class PlantsController < ApplicationController
+  # POST "gardens/:garden_id/plants/new"
   def create
+    # Here we need the garden_id to find the garden and associate it to the plant.
+    # This is why, in our routes.rb, we nested the :plants resources inside the
+    # :gardens resources only for the :create action
     @plant = Plant.new(plant_params)
     @garden = Garden.find(params[:garden_id])
     @plant.garden = @garden
@@ -7,12 +11,21 @@ class PlantsController < ApplicationController
     if @plant.save
       redirect_to garden_path(@garden)
     else
+      # Here we render "gardens/show" instead of rendering directly :show.
+      # This is normal: as we are in the PlantsController, rendering :show would
+      # render the show.html.erb of the '/views/plants' folder.
+      # We are obliged to specify to the PlantsController that it must render the
+      # show.html.erb which is inside the 'views/gardens' folder.
       render "gardens/show"
     end
   end
 
+  # DELETE "plants/:id"
   def destroy
     plant = Plant.find(params[:id])
+    # Before deleting the plant, we must store the garden of the plant in a variable
+    # so that we can redirect the user to the garden' show thanks to the
+    # garden_path helper.
     garden = plant.garden
     plant.destroy
     redirect_to garden_path(garden)
