b7fb8b47560aeeee32123783bf17bf198190a996
Guillaume Jauffret <guillaumejfrt@gmail.com>
2018-11-22 12:02:15 +0100

    explanations in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
   resources :gardens do
+    # We need a garden_id in the create action of the PlantsController to
+    # create a plant associated to its garden.
+
+    # So we need the garden_id to be in the params.
+
+    # So we nest the resources :plants inside the resources :gardens (for the
+    # create action only) to get the garden_id in the url.
     resources :plants, only: :create
   end
 
+  # In the destroy action of the PlantsController we don't need a garden_id to
+  # find the plant in DB. We only need the plant_id for that.
+
+  # So we don't nest the resources :plants for the destroy action.
   resources :plants, only: :destroy
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
